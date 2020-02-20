697fd444422a728ac4340b25a86dee9790ca8fce
Lucas Montano <lucasmontano@gmail.com>
2020-02-20 12:01:14 +0100

    creating simple single activity navigation with map

---
+++ app/src/main/java/com/lucasmontano/openweathermap/MapsFragment.kt
@@ -0,0 +1,46 @@
+package com.lucasmontano.openweathermap
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsFragment : Fragment(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_maps, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val mapFragment = SupportMapFragment.newInstance()
+        val fragmentTransaction: FragmentTransaction = fragmentManager!!.beginTransaction()
+        fragmentTransaction.add(R.id.mapContainerLayout, mapFragment)
+        fragmentTransaction.commit()
+
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a sample marker in Amsterdam
+        val sydney = LatLng(52.0, 4.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Amsterdam"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+}
